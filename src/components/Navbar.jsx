27b581d9d1be8e1acc7dfa60ea2ffd4af03ff56a
khalilurehman-masood <khalilurehman76@gmail.com>
2025-06-16 06:44:03 +0500

    added github profile link on the top left corner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = ({menuOpen, setMenuOpen}) => {
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    <a href="#home" className="font-mono text-xl font-bold text-white "><span className="text-blue-500">Khalil.tech</span></a>
+                    <a href="https://github.com/khalilurehman-masood" className="font-mono text-xl font-bold text-white "><span className="text-blue-500">🐙 Go to my GitHub</span></a>
                     <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={()=>setMenuOpen(!menuOpen)}>
                         {menuOpen ? "" : "☰"}
                     </div>
